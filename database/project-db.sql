4a82e087c9d24aca0b66264e9f86f158808ba6c5
Flamingraptor_70 <strubo.gt@gmail.com>
2023-11-14 14:32:07 +0200

    Added project database file

---
+++ database/project-db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE ice_project;
+
+USE ice_project;
+
+CREATE TABLE Users (
+    user_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    
+)
+
+CREATE TABLE Catalog (
+
+)
+
+CREATE TABLE Orders (
+
+)
+
+CREATE TABLE Cart (
+
+)
