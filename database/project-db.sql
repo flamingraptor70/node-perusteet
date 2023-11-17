4a7ea72e9e1e7c1de84f087800593e25fefb4526
Khai Chan <strubo.gt@gmail.com>
2023-11-17 23:31:32 +0200

    added project.db fil

--- database/project-db.sql
+++ database/project-db.sql
@@ -1,20 +1,48 @@
-CREATE DATABASE ice_project;
+-- Create database
 
-USE ice_project;
+DROP DATABASE IF EXISTS ice_dude;
+CREATE DATABASE ice_dude;
+USE ice_dude;
 
+-- Tables
+
+-- Table for Users
 CREATE TABLE Users (
-    user_id VARCHAR(255) NOT NULL PRIMARY KEY,
-    
-)
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Address VARCHAR(255),
+    CONSTRAINT unique_username UNIQUE (Username),
+    CONSTRAINT unique_email UNIQUE (Email)
+);
 
-CREATE TABLE Catalog (
+-- Table for Ice Cream Flavors
+CREATE TABLE IceCreamFlavors (
+    FlavorID INT PRIMARY KEY AUTO_INCREMENT,
+    FlavorName VARCHAR(255) NOT NULL,
+    Price DECIMAL(8, 2) NOT NULL
+);
 
-)
 
+-- Table for Ice Cream Orders
 CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT,
+    OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
 
-)
+-- Table for Ice Cream Order Items
+CREATE TABLE OrderItems (
+    OrderItemID INT PRIMARY KEY AUTO_INCREMENT,
+    OrderID INT,
+    FlavorID INT,
+    Quantity INT NOT NULL,
+    CONSTRAINT positive_quantity CHECK (Quantity > 0),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (FlavorID) REFERENCES IceCreamFlavors(FlavorID)
+);
 
-CREATE TABLE Cart (
-
-)
